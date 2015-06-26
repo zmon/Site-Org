8041d7950fe3620db524ea97751bffb301cf44b6
James Hazard <james.g.hazard@gmail.com>
2015-06-26 08:25:59 +0200

    improved names of some files and put the remarking of open fields

--- ccmac/library/print.php
+++ ccmac/library/print.php
@@ -3,18 +3,12 @@
 
 echo "<a href=index.php?action=source&file=" .$dir.">Source</a><br><br>";
 
-echo "<a href=index.php?action=source&file=" .$dir.">Source</a><br><br>";
-
 $lib_path = LIB_PATH;
 $document = `perl $lib_path/parser-print.pl $path/$dir`;
 
 $minDocLength = 1;
 
 if (strlen($document) > $minDocLength){ 
-
-$document=str_replace("{","<font color='red'>{",$document);
-
-$document=str_replace("}","}</font>",$document);
 
 //kludge to let html headers have formatting.
 
