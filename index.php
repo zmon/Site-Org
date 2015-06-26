e44d4119bf1a36bca7dbca98ff2073ca021c0a9b
Paul D Barham <paulb@savagesoft.com>
2015-06-25 22:59:22 -0500

    Trying a better way to define LIB_PATH
    This is the file system path to where the library is for the code.

--- index.php
+++ index.php
@@ -1,6 +1,7 @@
 <?php 
 
-DEFINE('LIB_PATH',dirname(__FILE__) . '/ccmac/library');					// Create static to our library	
+DEFINE('ROOT' ,dirname(__FILE__) );									// Root directory of site
+DEFINE('LIB_PATH', ROOT . '/ccmac/library');								// Create static to our library	
 ini_set('include_path', ini_get('include_path') . PATH_SEPARATOR . LIB_PATH . DIRECTORY_SEPARATOR);	// Include it in PHP include/require search
 
 include("ccmac_helpers.php"); 
